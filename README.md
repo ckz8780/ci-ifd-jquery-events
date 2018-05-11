a29d5ff362b7c1622fbb42101bbdeb68ac4d2b63
Chris Zielinski <ckz8780@gmail.com>
2018-05-11 01:35:17 +0000

    completed first round of challenges

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Here are a few examples of handling events with jQuery. This is part of the Interactive Frontend Development Module for CodeInstitute.
 
+The code uses boilerplate html/css files provided by CodeInstitute. Personal code is noted in script.js.
+
 The index is at https://ckz8780.github.io/ci-ifd-jquery-events/index.html
--- script.js
+++ script.js
@@ -19,6 +19,30 @@ $(document).ready(function() {
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream3").addClass('highlight_stream');
 	});
-
+	
+	
+	// Challenge code below this line
+	
+	$("p").click(function() {
+		$("p").css("color", "red");
+	});
+	
+	$("h2").mouseenter(function() {
+		$("h2").css("background-color", "lightblue");
+		$(this).addClass("bigh2");
+	});
+	
+	$("h2").mouseleave(function() {
+		$("h2").css("background-color", "white");
+		$(this).removeClass("bigh2");
+	});
+	
+	$(".bottom_button").mouseenter(function() {
+		$('body').css("background-color", "#000");
+	});
+	
+	$(".bottom_button").mouseleave(function() {
+		$('body').css("background-color", "#eee");
+	});
 
 }); 
